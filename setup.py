apljungquist/sprig

Enable type checking for distributed package
Unfortunately, to my knowledge, it is not possible to run the type checking on the
built and installed package, see github issue below for details:

https://github.com/python/mypy/issues/7087

setup.py
@@ -19,6 +19,8 @@ setuptools.setup(
     license="MIT",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
+    package_data={"sprig": ["py.typed"],},
+    zip_safe=False,
     install_requires=["more_itertools", "typing_extensions",],
     classifiers=[
         "Development Status :: 3 - Alpha",

apljungquist/lazylfs

setup.py
@@ -30,6 +30,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/apljungquist/lazylfs",
     packages=setuptools.find_packages("src"),
+    package_data={"lazylfs": ["py.typed"],},
+    zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",